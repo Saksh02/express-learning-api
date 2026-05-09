ef3d49c7f1f5968f82ce7c31b1c05e4450ce85bc
Saksh02 <ss.sakshamsrivastava02@gmail.com>
2026-05-09 12:34:07 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.post('/items', (req, res) => {
     if(data.name === undefined){
         return res.status(400).json({error: "Name is required"});
     }
+
     if(typeof data.name !== "string"){
         return res.status(400).json({error: "Name must be a string"});
     }
+    
     if(data.name.trim() === ""){
         return res.status(400).json({error: "Name cannot be empty"})
     }
